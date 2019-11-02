b0a699a1236f7a8027d16595337896de891c2161
makanai5610 <takumi.k.5610@gmail.com>
2019-11-02 21:41:05 +0900

    parse date

--- command/git_log.go
+++ command/git_log.go
@@ -6,7 +6,7 @@ import (
 
 func ExecGitLog(branches []string) ([]string, error) {
 	var branchLogs []string
-	dateFormat := "--date=short"
+	dateFormat := "--date=iso-strict"
 	for _, branch := range branches {
 		logFormat := "--pretty=format:{%n  \"branch_name\": \"" + branch + "\",%n  \"commiter_name\": \"%aN\",%n  \"last_commit_date\": \"%ad\"%n}"
 		branchLog, err := exec.Command("git", "log", "-n", "1", branch, logFormat, dateFormat).Output()
--- model/branch_info.go
+++ model/branch_info.go
@@ -2,12 +2,30 @@ package model
 
 import (
 	"encoding/json"
+	"strings"
+	"time"
 )
 
+type CustomDateTime struct {
+	time.Time
+}
+
+func (cdt *CustomDateTime) UnmarshalJSON(input []byte) error {
+	strInput := string(input)
+	strInput = strings.Trim(strInput, `"`)
+	newTime, err := time.Parse("2006-01-02T15:04:05-07:00", strInput)
+	if err != nil {
+		return err
+	}
+
+	cdt.Time = newTime
+	return nil
+}
+
 type BranchInfo struct {
-	BranchName           string `json:"branch_name"`
-	CommiterName         string `json:"commiter_name"`
-	LastCommitDateString string `json:"last_commit_date"`
+	BranchName     string         `json:"branch_name"`
+	CommiterName   string         `json:"commiter_name"`
+	LastCommitDate CustomDateTime `json:"last_commit_date"`
 }
 
 func New(gitLog string) BranchInfo {
