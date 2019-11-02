0b4494894a0578640df620c6ffe9a0236ccf7bf0
makanai5610 <takumi.k.5610@gmail.com>
2019-11-02 16:41:46 +0900

    [add] use git command and parse

--- main.go
+++ main.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"branch-purge-list-creator/command"
+	"branch-purge-list-creator/model"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
-	branches, err := execGitBranch()
+	branches, err := command.ExecGitBranch()
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Println(branches)
+	for _, branch := range branches {
+		fmt.Println(branch)
+	}
+
+	logs, err := command.ExecGitLog(branches)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, log := range logs {
+		fmt.Println(log)
+	}
+
+	var branchInfos []model.BranchInfo
+	for _, log := range logs {
+		branchInfos = append(branchInfos, model.New(log))
+	}
+
+	for _, branchInfo := range branchInfos {
+		fmt.Printf("%+v\n", branchInfo)
+	}
+
+	filteredBranchInfo := filterBranchInfo(branchInfos)
+	_ = filteredBranchInfo
+}
+
+func filterBranchInfo(branchInfos []model.BranchInfo) []model.BranchInfo {
+	now := time.Now().UTC()
+	var filteredBranchInfos []model.BranchInfo
+	for _, branchInfo := range branchInfos {
+		lastCommitDate, err := time.Parse(branchInfo.LastCommitDateString, "2006-01-02")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		days := int(now.Sub(lastCommitDate).Hours())
+		if days >= 14 {
+			filteredBranchInfos = append(filteredBranchInfos, branchInfo)
+		}
+	}
+	return filteredBranchInfos
 }
